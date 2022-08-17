f52328fb6cca497a7b061b847a3908098c7a9b22
fifa1219 <94551961+fifa1219@users.noreply.github.com>
2022-08-17 21:00:29 +0700

    Update file-ren.ps1

--- Kemampuan Dasar/Kemampuan Dasar 1/Kasus/file-ren.ps1
+++ Kemampuan Dasar/Kemampuan Dasar 1/Kasus/file-ren.ps1
@@ -1,6 +1,6 @@
 ﻿Get-ChildItem -Path C:\Users\Administrator\Desktop\*.java -Recurse -Force
 $confirmation = Read-Host "Do you want to continue?"
 if ($confirmation -eq 'y') {
-$name = Read-Host -Prompt "Enter Your Name"
+$name = Read-Host -Prompt "Enter New Name"
 Get-ChildItem -Path C:\Users\Administrator\Desktop\*.java -Recurse -Force | %{$x=0} {Rename-Item $_ -NewName $name ; $x++ }
 Get-ChildItem -Path C:\Users\Administrator\Desktop\*.java -Recurse -Force}
